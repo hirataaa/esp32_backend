ef11dba18fed790c493eccb1e7b332d8c5cf72d7
hirataaa <46036493+hirataaa@users.noreply.github.com>
2020-07-07 09:01:09 +0900

    make a data-point per 10mins (x4 types)

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const url = "mongodb://" + "127.0.0.1/" + dbName
 // get all data
 app.get('/data', function (req, res) {
     console.log("get all data")
-    MongoClient.connect(url, connectOption, function(err, db) {
+    MongoClient.connect(url, function(err, db) {
         db.db(dbName).collection(colName).find().sort().toArray(function(err, result) {
           result = result.slice(0,500)
           res.send(result)
@@ -27,27 +27,30 @@ app.get('/data', function (req, res) {
     })
 });
 
-// post data from esp32
-app.post('/post_data', function (req, res) {
+// 記事の内容更新. Article.vue
+app.post('/data', function (req, res) {
   console.log(req.body)
-  var insert_data = {
-    date : new Date(),
-    temperature : req.body.temperature,
-    humidity : req.body.humidity,
-    weight : req.body.weight
-  }
-  MongoClient.connect(url, connectOption, function(err, db) { 
-    db.db(dbName).collection(colName).insertOne(insert_data, function(err, result) {
-      console.log("1 document inserted");
-      res.send(result.value)
-      db.close();
-    });
+
+  var year  = new Date().getFullYear();
+  var month = new Date().getMonth();
+  var day = new Date().getDate();
+  var hours = new Date().getHours();
+  var minutes = Math.floor( new Date().getMinutes() / 10 ) * 10; // 分の小数点切り捨て
+
+  var query_date = new Date(year,month,day,hours,minutes)
+  console.log(year,month,day,hours,minutes)
+
+  var insert_data = Object.assign({ date : query_date }, req.body);
+
+  // 10分間来たデータは同じデータ点として登録．
+  MongoClient.connect(url, function(err, db) {
+  db.db(dbName).collection(colName)
+  .updateOne({ date : query_date }, { $set:  insert_data }, {returnOriginal: false,upsert: true} , function(err, result) {
+    db.close();
+    console.log(result)
+    res.send(result)
   });
+}); 
 });
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
